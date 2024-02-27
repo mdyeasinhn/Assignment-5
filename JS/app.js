e7c511ebbb5f33e32e229e16774adf4a262f66a8
Md Yeasin <mdyeasin234098@gmail.com>
2024-02-27 11:39:14 +0600

    seat apped commplete

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,16 @@
-const allBtn =document.getElementsByClassName('btn');
+const allBtn =document.getElementsByClassName('btn-green');
+const selectSeat=document.getElementById('select-seat');
 let count = 0;
+let countSelectSeat = 40;
 for (const btn of allBtn) {
     btn.addEventListener('click',function(e) {
         e.target.style.backgroundColor="green";
         count++;
+        countSelectSeat=countSelectSeat-1
 
-        console.log();
          const seatName = e.target.parentNode.childNodes[3].innerText;
          const price = 550 ;
-         const selectSeat=document.getElementById('select-seat');
+ 
         const li =document.createElement('li');
         const p =document.createElement('p');
         p.innerText=seatName;
@@ -20,20 +22,42 @@ for (const btn of allBtn) {
         li.appendChild(p2);
         li.appendChild(p3);
         selectSeat.appendChild(li);
-        const totalCost=document.getElementById("total-cost");
+        const totalCost=document.getElementById("total-cost").innerText;
         console.log( typeof parseInt(totalCost));
         const convertedTotalCost=parseInt(totalCost);
         document.getElementById("total-cost").innerText=convertedTotalCost+ parseInt(price);
-        setInnerText('seat-count', count);
+        console.log(count)
+        setInnerText('total-seat',countSelectSeat);
+        setInnerText("seat-count", count);
         // const select = e.target.innerText;
     }
     
     )
     
 }
+
+function hideElementById(elementId){
+    const element =document.getElementById( elementId);
+    element.classList.add("hidden");
+}
 function setInnerText(id,value) {
     document.getElementById(id).innerText = value;
 
 }
-// const seatName = document.getElementById('seat-name');
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400')
+}
+
+function getTextElementValueById(elementId) {
+    const element= document.getElementById(elementId);
+    const elementValueText=element.innerText;
+    const value=parseInt(elementValueText);
+    return value ;
+}
+
+function setTextElementValueById(elementId, value) {
+    const element =document.getElementById(elementId);
+    element.innerText = value;
+}
 
